8827a4bde2f56e04595e1d7a9561d198fb422907
Cedric Krause <cedric.krause@uni-due.de>
2021-07-02 11:46:53 +0200

    Remove bootstrap package as it interferes with bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
