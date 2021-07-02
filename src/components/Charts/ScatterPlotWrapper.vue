b768f7df7799401d6dc9b34de9bfc89990086909
Cedric Krause <cedric.krause@uni-due.de>
2021-07-02 12:27:20 +0200

    ScatterplotWrapper selections corrected layout and hotloads the Scatterplot when selections are made

--- src/components/Charts/ScatterPlotWrapper.vue
+++ src/components/Charts/ScatterPlotWrapper.vue
@@ -1,10 +1,50 @@
 <template>
   <div>
-    <b-form-select v-model="xAxisColumn" :options="dataset.columns"></b-form-select>
-    <b-form-select v-model="yAxisColumn" :options="dataset.columns"></b-form-select>
-    <b-form-select v-model="radiusColumn" :options="dataset.columns"></b-form-select>
-    <b-button variant="primary" v-on:click="fire()">Go</b-button>
-    <scatter-plot v-if="go"
+      <b-container>
+        <b-row>
+          <b-col>
+            <b-form-group
+              id="input-group-1"
+              label="X-Axis:"
+              label-for="xAxisInput"
+            >
+              <b-form-select
+                id="xAxisInput"
+                v-model="xAxisColumn"
+                :options="dataset.columns"
+                required></b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-group
+              id="input-group-2"
+              label="Y-Axis:"
+              label-for="yAxisInput"
+            >
+              <b-form-select
+                id="yAxisInput"
+                v-model="yAxisColumn"
+                :options="dataset.columns"
+                required></b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-group
+              id="input-group-3"
+              label="Radius:"
+              label-for="radiusInput"
+            >
+              <b-form-select
+                id="radiusInput"
+                v-model="radiusColumn"
+                :options="dataset.columns"
+                required></b-form-select>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-container>
+
+    <scatter-plot v-if="this.xAxisColumn && this.yAxisColumn && this.radiusColumn"
     :dataset="dataset"
     :xAxisColumn="xAxisColumn"
     :yAxisColumn="yAxisColumn"
@@ -25,16 +65,10 @@ export default Vue.extend({
   },
   data() {
     return {
-      go: false,
       xAxisColumn: null,
       yAxisColumn: null,
       radiusColumn: null,
     };
-  },
-  methods: {
-    fire() {
-      this.go = true;
-    },
   },
 });
 </script>
