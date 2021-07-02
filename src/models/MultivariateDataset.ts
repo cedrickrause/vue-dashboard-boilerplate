a86780bccf0024084ad55688b4e313ce9265e05d
Cedric Krause <cedric.krause@uni-due.de>
2021-07-02 12:29:16 +0200

    MultivariateDataset properties cannot be null

--- src/models/MultivariateDataset.ts
+++ src/models/MultivariateDataset.ts
@@ -5,14 +5,14 @@ type MultivariateDatasetEntry = {
  };
 
 export interface MultivariateDataset {
-  data: Array<MultivariateDatasetEntry> | null,
-  columns: Array<string> | null,
+  data: Array<MultivariateDatasetEntry>,
+  columns: Array<string>,
 }
 
 export class MultivariateDatasetImpl implements MultivariateDataset {
-  data: Array<MultivariateDatasetEntry> | null;
+  data: Array<MultivariateDatasetEntry>;
 
-  columns: Array<string> | null;
+  columns: Array<string>;
 
   constructor(data: DSVRowArray<string>, columns: Array<string>) {
     this.data = data.map((entry) => entry);
