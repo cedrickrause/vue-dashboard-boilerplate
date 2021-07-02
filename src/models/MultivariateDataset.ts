7847df24bb10b61263890539f95b72e1912e52f5
Cedric Krause <cedric.krause@uni-due.de>
2021-07-02 20:33:37 +0200

    Introduce BaseDataset interface for all other dataset interfaces to extend

--- src/models/MultivariateDataset.ts
+++ src/models/MultivariateDataset.ts
@@ -1,10 +1,11 @@
 import { DSVRowArray } from 'd3';
+import { BaseDataset } from './BaseDataset';
 
 type MultivariateDatasetEntry = {
   [key: string]: string | undefined,
  };
 
-export interface MultivariateDataset {
+export interface MultivariateDataset extends BaseDataset {
   data: Array<MultivariateDatasetEntry>,
   columns: Array<string>,
   idColumn: string,
