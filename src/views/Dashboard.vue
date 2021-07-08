7e4b9a190d4a3fefb272483457f1e2cc423d637e
Cedric Krause <cedric.krause@uni-due.de>
2021-07-08 14:49:45 +0200

    Card of chart without wrapper made the same size to avoid ugly whitespace

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,18 +2,24 @@
   <div class="dashboard">
     <b-container fluid>
       <b-row align-h="center">
-        <b-col cols="4">
-          <b-card>
+        <b-col cols="3">
+          <b-card
+            title="Scatterplot with Wrapper">
             <scatter-plot-wrapper v-if="getData" :dataset="getData" />
           </b-card>
         </b-col>
-        <b-col cols="4">
-          <b-card>
+        <b-col cols="3">
+          <b-card class="h-100"
+            title="Scatterplot without Wrapper">
             <scatter-plot v-if="getData"
               :dataset="getData"
               :xAxisColumn="'mpg'"
               :yAxisColumn="'hp'"
               :radiusColumn="'disp'" />
+              <b-card-text>
+                Because this card's content is smaller than the one with the wrapper,
+                the class h-100 is added to make the card fill out the whole row height.
+              </b-card-text>
           </b-card>
         </b-col>
       </b-row>
